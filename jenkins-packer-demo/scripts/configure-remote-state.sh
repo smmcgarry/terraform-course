f938b981ffebe173ad20d76c7b26237c9fa63430
smmcgarry <37545523+smmcgarry@users.noreply.github.com>
2018-04-23 21:59:56 +0100

    Delete configure-remote-state.sh
    using backend.tf

--- jenkins-packer-demo/scripts/configure-remote-state.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# not necessary anymore in newer terraform versions, you can use a backend.tf file
-if [ "`terraform --version |head -n1`" == "Terraform v0.7.7" ] ; then
-  terraform remote config -backend=s3 -backend-config="bucket=terraform-state-abc123a" -backend-config="key=terraform/terraform.tfstate" -backend-config="region=eu-west-1"
-fi
