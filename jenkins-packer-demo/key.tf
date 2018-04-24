13ddfefa4d5ac627ab8693ad0711643fd6d7123e
smmcgarry <37545523+smmcgarry@users.noreply.github.com>
2018-04-24 11:35:36 +0100

    Update key.tf

--- jenkins-packer-demo/key.tf
+++ jenkins-packer-demo/key.tf
@@ -1,5 +1,5 @@
-resource "aws_key_pair" "mykeypair" {
-  key_name = "mykeypair"
+resource "aws_key_pair" "mykeypair3" {
+  key_name = "mykeypair3"
   public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
   lifecycle {
     ignore_changes = ["public_key"]
